49a9bbac72a5dc22ee20cd90a352f92f2cf2ec75
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:42:44 +0100

    Cleanup tests

--- tests/test_sale_total.py
+++ tests/test_sale_total.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python
-#This file is part sale_total module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part sale_total module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT, test_view,\
@@ -18,23 +10,17 @@ from trytond.transaction import Transaction
 
 
 class SaleTotalTestCase(unittest.TestCase):
-    '''
-    Test Sale Total module.
-    '''
+    'Test Sale Total module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_total')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_total')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
