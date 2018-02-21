b65d5759d1a80138499a07e02646588da5a286a8
C?dric Krier <ced@b2ck.com>
2018-02-21 10:34:01 +0100

    Fix tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,9 @@
 # This file is part sale_total module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-
-from .test_sale_total import suite
+try:
+    from trytond.modules.sale_total.tests.test_sale_total import suite
+except ImportError:
+    from .test_sale_total import suite
 
 __all__ = ['suite']
