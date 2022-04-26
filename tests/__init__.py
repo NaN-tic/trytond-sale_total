4187ce083ec60ee0b7a81472493b74b2737892d7
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:12:21 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part sale_total module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-try:
-    from trytond.modules.sale_total.tests.test_sale_total import suite
-except ImportError:
-    from .test_sale_total import suite
-
-__all__ = ['suite']
